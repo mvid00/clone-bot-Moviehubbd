5c518a66a34bb15c2db426f2f955c264fdc6fe1c
taisirtf <110351570+taisirtf@users.noreply.github.com>
2022-09-19 00:51:33 +0600

    Update gdrive_helper.py

--- bot/helper/mirror/upload/gdrive_helper.py
+++ bot/helper/mirror/upload/gdrive_helper.py
@@ -213,7 +213,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             "name": file_name,
-            "description": "Uploaded by Mirror-leech-telegram-bot",
+            "description": "Uploaded by Moviehubbd-Clone-bot",
             "mimeType": mime_type,
         }
         if parent_id is not None:
